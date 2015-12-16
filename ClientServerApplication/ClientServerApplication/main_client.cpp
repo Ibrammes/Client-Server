1ece40cca181fa4024942848810acb34a8965e77
Ibrammes <balexar@mail.ru>
2015-12-16 19:06:55 +0300

    stress testing 1

--- ClientServerApplication/ClientServerApplication/main_client.cpp
+++ ClientServerApplication/ClientServerApplication/main_client.cpp
@@ -1,16 +1,28 @@
 #include "TalkToSvr.h"
+//#include <string>
+
+using namespace std;
 
 io_service service;
 
 int main()
 {
 	ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"), 8001);
-	char* messages[] = { "John says hi", "so does James", "Lucy got home", 0 };
+	string s, _i;
+	for (int i = 0; i < 300; ++i)
+	{
+		s = "Client " + to_string(i);
+		talk_to_svr::start(ep, s);
+
+		boost::this_thread::sleep(boost::posix_time::millisec(100));
+	}
+	
+	/*char* messages[] = { "John says hi", "so does James", "Lucy got home", 0 };
 	for (char ** message = messages; *message; ++message)
 	{
 		talk_to_svr::start(ep, *message);
 		boost::this_thread::sleep(boost::posix_time::millisec(100));
-	}
+	}*/
 	service.run();
 
 	std::getchar();
